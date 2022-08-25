2abc7184a1698a2dbccca6f866e7b74c9ed6dcc7
moranbental <moranb@iguazio.com>
2022-08-25 16:51:47 +0300

    change project directory + add setup file and dev-requirments

---
+++ relayer/_version.py
@@ -0,0 +1,2 @@
+"""Package version number"""
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,75 @@
+
+# Copyright 2020 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup, find_packages
+from encodings import utf_8
+import re
+
+PACKAGE_NAME = "relayer"
+VERSION_FILE = PACKAGE_NAME + '/_version.py'
+
+def version():
+    with open(VERSION_FILE) as fp:
+        for line in fp:
+            if line.startswith('__version__'):
+                _, version = line.split('=')
+                return version.replace("'", '').strip()
+        
+
+def load_deps(file_name):
+    """Load dependencies from requirements file"""
+    deps = []
+    with open(file_name) as fp:
+        for line in fp:
+            line = line.strip()
+            if not line or line[0] == '#' or line.startswith('-r'):
+                continue
+            deps.append(line)
+    return deps
+
+
+install_requires = load_deps('requirements.txt')
+tests_require = load_deps('dev-requirements.txt')
+
+with open("README.md", "r", encoding=utf_8) as fh:
+    long_description = fh.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=version(),
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Iguazio',
+    author_email='moranb@iguazio.com',
+    license='MIT',
+    url='https://github.com/v3io/v3iofs-py',
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries',
+    ],
+    tests_require=tests_require,
+)
